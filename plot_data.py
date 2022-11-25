47d3e43aa1c8253f5634d7e27998179e2c94e7bb
bernhardjonsson <bernton96@gmail.com>
2022-11-25 09:50:57 +0100

    suit exp set up

--- plot_data.py
+++ plot_data.py
@@ -1,7 +1,10 @@
 import pickle
 import tcav.utils_plot as utils_plot # utils_plot requires matplotlib
 
-with open('results.bin', 'rb') as f:
+file = 'fire_engin_results.bin'
+random_exp = 25
+
+with open(file, 'rb') as f:
     results = pickle.load(f)
 
-utils_plot.plot_results(results, num_random_exp=3)
+utils_plot.plot_results(results, num_random_exp=random_exp)
--- random_gen.py
+++ random_gen.py
@@ -11,20 +11,25 @@ from zipfile import ZipFile
 
 img_count = 0
 nr_of_img = nr_of_pic*(nr_of_random+1)
+used_idx = []
 
 with ZipFile(source, 'r') as zipObject:
    filename = zipObject.namelist()
    while img_count <= nr_of_img:
        img_idx = random.randint(0,len(filename))
-       if "Data/CLS-LOC/train" in filename[img_idx]:
+       if "Data/CLS-LOC/train" in filename[img_idx] and img_idx  not in used_idx:
            zipObject.extract(filename[img_idx], 'tmp')
            img_count +=1
+           used_idx.append(img_idx)
 
+print(img_count)
 copy_path=[]
 for root,dirs,files in os.walk("tmp"):
     for file in files:
         copy_path.append(os.path.join(root,file))
 random.shuffle(copy_path)
+print(copy_path[0:10])
+print(len(copy_path))
 img_idx = 0
 for i in range(nr_of_random+1):
     folder_name = "random500_" + str(i)
